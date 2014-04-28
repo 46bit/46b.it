450bfb37d01d7f3f98b5fc786aac19e2c72c31d1
Michael Mokrysz <hi@46bit.com>
2014-04-28 04:31:52 +0100

    Tweaked year archives.

--- _46bit.hs
+++ _46bit.hs
@@ -30,7 +30,7 @@ main = hakyllWith config $ do
 	match "index.html" $ do
 		route idRoute
 		compile $ do
-			posts <- recentFirst =<< loadAll "posts/*"
+			posts <- fmap (take 5) . recentFirst =<< loadAll "posts/*"
 			let indexCtx =
 				constField "title" "Home" `mappend`
 				listField "posts" postCtx (return posts) `mappend`
