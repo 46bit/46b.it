8149875ce2cbf2df56456a7d244ede9960d9edb7
Michael Mokrysz <hi@46b.it>
2018-05-19 01:46:59 +0100

    Final redraft of deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
+set -eu
+
 let release_number=`cat release | sed -e s/r//g`+1
 release="r$release_number"
 echo $release > ./release
 git add ./release
 
-git commit -m "Performing release $release"
+echo "Performing release $release"
+git commit -m "Releasing $release"
 git push
 git tag -a $release -m "Release $release"
 git push origin $release
 
-ssh sirius "cd /var/www/46b.it/_46bit && git fetch --tags && git checkout tags/$release"
+ssh -q sirius \
+  "cd /var/www/46b.it/_46bit && \
+   git fetch --tags && \
+   git checkout tags/$release"
